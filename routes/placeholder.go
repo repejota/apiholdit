733757423a557145fd1e91952132697cda5f4f9b
Raül Pérez <repejota@gmail.com>
2017-09-19 23:00:51 +0200

    Added bgcolor support

--- routes/placeholder.go
+++ routes/placeholder.go
@@ -14,35 +14,8 @@ import (
 	"strconv"
 )
 
-// writeImage encodes an image 'img' in PNG format and writes it
-// into ResponseWriter.
-func writeImage(w http.ResponseWriter, img *image.Image) {
-
-	buffer := new(bytes.Buffer)
-	if err := png.Encode(buffer, *img); err != nil {
-		log.Println("unable to encode image.")
-	}
-
-	w.Header().Set("Content-Type", "image/png")
-	w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
-	if _, err := w.Write(buffer.Bytes()); err != nil {
-		log.Println("unable to write image.")
-	}
-}
-
-// renderImage renders an image of specified size.
-func renderImage(width int, height int, color *color.RGBA) image.Image {
-	m := image.NewRGBA(image.Rect(0, 0, width, height))
-	draw.Draw(m, m.Bounds(), &image.Uniform{color}, image.ZP, draw.Src)
-
-	var img image.Image = m
-	return img
-}
-
 // getColor gets a color from a RGB HTML hex string.
 func getColor(colorstr string) (color.RGBA, error) {
-	fmt.Println(colorstr)
-
 	var col color.RGBA
 	format := "%02x%02x%02x"
 	var r, g, b uint8
@@ -59,27 +32,45 @@ func getColor(colorstr string) (color.RGBA, error) {
 	return col, nil
 }
 
+// renderImage renders an image of specified size.
+func renderImage(width int, height int, color *color.RGBA) image.Image {
+	canvas := image.NewRGBA(image.Rect(0, 0, width, height))
+	draw.Draw(canvas, canvas.Bounds(), &image.Uniform{color}, image.ZP, draw.Src)
+	var img image.Image = canvas
+	return img
+}
+
+// writeImage encodes an image 'img' in PNG format and writes it
+// into ResponseWriter.
+func writeImage(w http.ResponseWriter, img *image.Image) {
+	buffer := new(bytes.Buffer)
+	if err := png.Encode(buffer, *img); err != nil {
+		log.Println("unable to encode image.")
+	}
+	w.Header().Set("Content-Type", "image/png")
+	w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
+	if _, err := w.Write(buffer.Bytes()); err != nil {
+		log.Println("unable to write image.")
+	}
+}
+
 // PlaceHolder generates an image placeholder.
 func PlaceHolder(w http.ResponseWriter, r *http.Request) {
-
 	width, err := strconv.Atoi(r.URL.Query().Get("width"))
 	if err != nil {
 		http.Error(w, "Invalid image width", http.StatusBadRequest)
 		return
 	}
-
 	height, err := strconv.Atoi(r.URL.Query().Get("height"))
 	if err != nil {
 		http.Error(w, "Invalid image height", http.StatusBadRequest)
 		return
 	}
-
 	bgcolor, err := getColor(r.URL.Query().Get("bgcolor"))
 	if err != nil {
 		http.Error(w, "Invalid color", http.StatusBadRequest)
 		return
 	}
-
 	img := renderImage(width, height, &bgcolor)
 	writeImage(w, &img)
 }
