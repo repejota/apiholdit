5f0c0365cc97787f8aba6fda61b92e5f2a544785
Raül Pérez <repejota@gmail.com>
2017-09-19 18:49:46 +0200

    Use PNG

--- routes/placeholder.go
+++ routes/placeholder.go
@@ -7,28 +7,29 @@ import (
 	"image"
 	"image/color"
 	"image/draw"
-	"image/jpeg"
+	"image/png"
 	"log"
 	"net/http"
 	"strconv"
 )
 
-// writeImage encodes an image 'img' in jpeg format and writes it into ResponseWriter.
+// writeImage encodes an image 'img' in PNG format and writes it
+// into ResponseWriter.
 func writeImage(w http.ResponseWriter, img *image.Image) {
 
 	buffer := new(bytes.Buffer)
-	if err := jpeg.Encode(buffer, *img, nil); err != nil {
+	if err := png.Encode(buffer, *img); err != nil {
 		log.Println("unable to encode image.")
 	}
 
-	w.Header().Set("Content-Type", "image/jpeg")
+	w.Header().Set("Content-Type", "image/png")
 	w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
 	if _, err := w.Write(buffer.Bytes()); err != nil {
 		log.Println("unable to write image.")
 	}
 }
 
-// PlaceHolder ...
+// PlaceHolder generates an image placeholder.
 func PlaceHolder(w http.ResponseWriter, r *http.Request) {
 	m := image.NewRGBA(image.Rect(0, 0, 240, 240))
 	blue := color.RGBA{0, 0, 255, 255}
