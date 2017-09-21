442f8e52b671ce69ac6eabf5d7a6808481c37b57
Raül Pérez <repejota@gmail.com>
2017-09-21 20:18:12 +0200

    Better test for SetBgColor

--- apiholdit_test.go
+++ apiholdit_test.go
@@ -14,10 +14,19 @@ func TestNewPlaceHolder(t *testing.T) {
 	}
 }
 
-func TestSetBgColort(t *testing.T) {
+func TestSetBgColor(t *testing.T) {
 	placeholder := NewPlaceHolder(640, 480)
 	placeholder.SetBgColor("ff0000")
 	if placeholder.BackgroundColor.R != 255 {
 		t.Fatalf("Placeholder background color R expected to be 255 but got %d", placeholder.BackgroundColor.R)
+	}
+	if placeholder.BackgroundColor.G != 0 {
+		t.Fatalf("Placeholder background color R expected to be 0 but got %d", placeholder.BackgroundColor.G)
+	}
+	if placeholder.BackgroundColor.B != 0 {
+		t.Fatalf("Placeholder background color R expected to be 0 but got %d", placeholder.BackgroundColor.B)
+	}
+	if placeholder.BackgroundColor.A != 255 {
+		t.Fatalf("Placeholder background color A expected to be 255 but got %d", placeholder.BackgroundColor.A)
 	}
 }
