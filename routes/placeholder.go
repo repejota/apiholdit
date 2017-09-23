09079e76bd0afa5fe1f2a9edb957e2139e53d18d
Raül Pérez <repejota@gmail.com>
2017-09-23 03:12:09 +0200

    check for fgcolor query param

--- routes/placeholder.go
+++ routes/placeholder.go
@@ -51,11 +51,13 @@ func PlaceHolder(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	fgcolorstr := r.URL.Query().Get("fgcolor")
-	err := p.SetForegroundColor(fgcolorstr)
-	if err != nil {
-		http.Error(w, "Invalid placeholder foreground color", http.StatusBadRequest)
-		return
+	if _, ok := queryParams["fgcolor"]; ok {
+		fgcolorstr := r.URL.Query().Get("fgcolor")
+		err := p.SetForegroundColor(fgcolorstr)
+		if err != nil {
+			http.Error(w, "Invalid placeholder foreground color", http.StatusBadRequest)
+			return
+		}
 	}
 
 	err = p.SetText("Lorem ipsum dolor sit amet.")
