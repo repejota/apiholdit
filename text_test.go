fbdab79884b4fc94a03db3b0aad1df62f0d1bfb9
Raül Pérez <repejota@gmail.com>
2017-09-23 03:35:47 +0200

    Initial test getFont

--- text_test.go
+++ text_test.go
@@ -1,3 +1,12 @@
 // Copyright 2017 The apiholdit Authors. All rights reserved.
 
 package apiholdit
+
+import "testing"
+
+func TestGetFont(t *testing.T) {
+	_, err := getFont()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
